0f50b306142cc6a60e7c0372e9b875a6620a5bf8
Ajithkumar10061998 <135553045+Ajithkumar10061998@users.noreply.github.com>
2023-07-16 16:51:33 +0530

    Update Integration.java

--- Integration.java
+++ Integration.java
@@ -2,7 +2,7 @@
 public class Integration {
 
 public static void main(String[] args) {
-	
+	//jenkins testing
 System.out.println("continuous Integration CI");
 System.out.println("continuous deployment CD");
 
