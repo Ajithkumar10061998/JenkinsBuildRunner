4f8d27cc33b6dda7f24c1e113519245df86f0d35
Ajithkumar10061998 <135553045+Ajithkumar10061998@users.noreply.github.com>
2023-07-16 17:37:20 +0530

    url with githubwebhook

--- Integration.java
+++ Integration.java
@@ -2,7 +2,7 @@
 public class Integration {
 
 public static void main(String[] args) {
-//webhook added
+//webhook added successfully
 System.out.println("continuous Integration CI");
 System.out.println("continuous deployment CD");
 
