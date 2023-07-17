5b3a91a8f05d8c58985d72d4f5149fd6ea353cbe
Ajithkumar10061998 <135553045+Ajithkumar10061998@users.noreply.github.com>
2023-07-17 19:25:19 +0530

    deleted WebHook

--- WebHook.java
+++ WebHook.java
@@ -5,7 +5,7 @@ public static void main(String[] args) {
 
 System.out.println("continuous Integration CI");
 System.out.println("continuous deployment CD");
-System.out.println("webhooked under setting with github-webhook");
+System.out.println("webhooked under setting ");
   
 }
 }
