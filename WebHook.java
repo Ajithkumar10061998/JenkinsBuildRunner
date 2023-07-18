9e196ce9ec6b47c4cd61150f76f2e32f9dfde174
Ajithkumar10061998 <135553045+Ajithkumar10061998@users.noreply.github.com>
2023-07-18 21:01:25 +0530

    Update main branch

--- WebHook.java
+++ WebHook.java
@@ -5,7 +5,7 @@ public static void main(String[] args) {
 
 System.out.println("continuous Integration CI");
 System.out.println("continuous deployment CD");
-System.out.println("webhooked under error free setting ");
+System.out.println("webhooked under error free setting by adding main branch ");
   
 }
 }
